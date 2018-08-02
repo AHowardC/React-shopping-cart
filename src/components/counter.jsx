6edeb60aaf6b6fc773fdcfdc17635b4ba1e76df3
Akil <achsvgusaeng@yahoo.com>
2018-08-02 15:51:18 +0000

    updated counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 1,
-        // imageUrl: 'https://picsum.photos/201'
+        count: 0,
+        tags: ['tag1', 'tag2', 'tag3'],
+        imageUrl: 'https://picsum.photos/201',
+
     };
 
     styles = {
@@ -11,12 +13,22 @@ class Counter extends Component {
         fontWeight: "bold"
     };
 
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+    }
+
     render() {
         return (
             <React.Fragment>
                 <img src={this.state.imageUrl} alt=""/>
                 <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
+                <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+                <div>
+                {this.state.tags.length === 0 && "Please create a new tag!"}
+                {this.renderTags()}</div>
             </React.Fragment>
         );
     }
