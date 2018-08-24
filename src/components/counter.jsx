a33f270b892111bc2c4972f5c9e7fc264f976b8d
Akil <achsvgusaeng@yahoo.com>
2018-08-24 16:23:07 +0000

    added navbar

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,58 +1,32 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = {
-        count: 0,
-        tags: ['tag1', 'tag2', 'tag3'],
-        imageUrl: 'https://picsum.photos/201',
-    };
-
-    // constructor() {
-    //     super();
-    //     this.handleIncrement = this.handleIncrement.bind(this);
-    // }
-
-    styles = {
-        fontSize: '50px',
-        fontWeight: "bold"
-    };
-
-    renderTags() {
-        if (this.state.tags.length === 0) return <p>There are no tags</p>;
-
-        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
-    }
-
-    // when you need to bind this keyword in a class use es6 arrow function so you dont need to set up a constructor to bind this keyword for eventhandlers
-
-    handleIncrement = () => {
-        console.log('Increment Clicked', this);
-    }
-
     render() {
         return (
             <React.Fragment>
-                <img src={this.state.imageUrl} alt=""/>
                 <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                
                 <button 
-                onClick={this.handleIncrement} 
-                className="btn btn-secondary btn-sm">Increment</button>
-                <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
-                <div>
-                {this.state.tags.length === 0 && "Please create a new tag!"}
-                {this.renderTags()}</div>
+                    onClick={()=> this.props.onIncrement(this.props.counter)}
+                    className="btn btn-secondary btn-sm">Increment
+                </button>
+                
+                <button 
+                    onClick={()=>this.props.onDelete(this.props.counter)} 
+                    className="btn btn-danger btn-sm m-2">Delete
+                </button>
             </React.Fragment>
         );
     }
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        classes += (this.props.counter === 0) ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-        const { count } = this.state;
+        const { count } = this.props.counter;
         return count === 0 ? 'Zero' : count;
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter';
 
 
 
-
-
-// let element = <h1>Hello World</h1>;
-ReactDOM.render(<Counter/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
